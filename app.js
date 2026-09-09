7b56d7b2bf837be48c3c5e884318383fd062ad8a
Jav991 <jgomezcomino04@gmail.com>
2026-09-09 08:59:34 +0200

    añadir configuración inicial de la aplicación y Docker

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) =>{
+    res.json({
+        system: 'PANTOM-01',
+        status: 'ONLINE',
+        envioronment: 'SIMULATION',
+        version: '1.0.0' 
+    });
+});
+
+app.get('/objetivos', (req, res) => {
+    res.json([
+        {
+            name: 'Pedro Sánchez',
+            codname: 'OBJ-001',
+            status: 'MONITORED',
+            riskLevel: 'HIGH'
+        },
+        {
+            name: 'Isabel Díaz Ayuso',
+            codname: 'OBJ-002',
+            status: 'MONITORIED',
+            reisklevel: 'HIGH'
+        }
+    ]);
+});
+
+app.listen(PORT, () =>{
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
+});
